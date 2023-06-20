fc318e6a28ea5c803ffdc380e5ef5e5ecf57633a
ChrisKent <chrisgkent@gmail.com>
2023-06-20 12:38:33 +0100

    Added cache of ends/starts

--- primal_digest/classes.py
+++ primal_digest/classes.py
@@ -2,19 +2,22 @@ from primal_digest.config import AMBIGUOUS_DNA_COMPLEMENT
 from primal_digest.iteraction import all_inter_checker
 from primal_digest.primer_pair_score import ol_pp_score, walk_pp_score
 
+
 class FKmer:
     end: int
     seqs: set[str]
+    _starts: set[int]
 
     def __init__(self, end, seqs) -> None:
         self.end = end
         self.seqs = seqs
+        self._starts = {self.end - len(x) for x in self.seqs}
 
     def len(self) -> set[int]:
         return {len(x) for x in self.seqs}
 
     def starts(self) -> set[int]:
-        return {self.end - len(x) for x in self.seqs}
+        return self._starts
 
     def __str__(self, referance, amplicon_prefix, pool) -> str:
         string_list = []
@@ -36,21 +39,25 @@ class FKmer:
     def __eq__(self, other):
         if isinstance(other, FKmer):
             return self.__hash__() == other.__hash__()
+        else:
+            return False
 
 
 class RKmer:
     start: int
     seqs: set[str]
+    _ends: set[int]
 
     def __init__(self, start, seqs) -> None:
         self.start = start
         self.seqs = seqs
+        self._ends = {len(x) + self.start for x in self.seqs}
 
     def len(self) -> set[int]:
         return {len(x) for x in self.seqs}
 
     def ends(self) -> set[int]:
-        return {len(x) + self.start for x in self.seqs}
+        return self._ends
 
     def __str__(self, referance, amplicon_prefix, pool) -> str:
         string_list = []
@@ -80,6 +87,8 @@ class RKmer:
     def __eq__(self, other):
         if isinstance(other, RKmer):
             return self.__hash__() == other.__hash__()
+        else:
+            return False
 
 
 class PrimerPair:
@@ -113,7 +122,7 @@ class PrimerPair:
         True means interaction
         """
         return all_inter_checker(self.fprimer.seqs, self.rprimer.seqs, cfg=cfg)
- 
+
     def all_seqs(self) -> set[str]:
         return [x for x in self.fprimer.seqs] + [x for x in self.rprimer.seqs]
 
@@ -130,36 +139,41 @@ class PrimerPair:
             amplicon_prefix=f"{amplicon_prefix}_{self.amplicon_number}",
             pool=self.pool + 1,
         )
-    
 
-class Scheme():
-    _pools : list[list[PrimerPair]]
+
+class Scheme:
+    _pools: list[list[PrimerPair]]
     _current_pool: int
     npools: int
     _last_pp_added: PrimerPair
     cfg: dict
 
-    def __init__(self,cfg):
+    def __init__(self, cfg):
         self.n_pools = cfg["npools"]
         self._pools = [[] for _ in range(self.n_pools)]
         self._current_pool = 0
         self._pp_number = 1
         self.cfg = cfg
-        
-    
+
     @property
     def npools(self) -> int:
         return self.n_pools
-    
+
     def next_pool(self) -> int:
         return (self._current_pool + 1) % self.n_pools
-    
+
     def add_primer_pair_to_pool(self, primerpair, pool, msa_index):
         primerpair.pool = pool
         primerpair.msa_index = msa_index
-        primerpair.amplicon_number = len([pp for sublist in self._pools for pp in sublist if pp.msa_index == msa_index])
-        
-        
+        primerpair.amplicon_number = len(
+            [
+                pp
+                for sublist in self._pools
+                for pp in sublist
+                if pp.msa_index == msa_index
+            ]
+        )
+
         # Adds the primerpair to the spesified pool
         self._pools[pool].append(primerpair)
         self._current_pool = self.next_pool()
@@ -168,21 +182,25 @@ class Scheme():
     def add_primer_pair(self, primerpair, msa_index):
         "Adds primerpair to the current pool, and updates the current pool"
         # Adds the primerpair to the current pool
-        self.add_primer_pair_to_pool(primerpair, self._current_pool,msa_index)
+        self.add_primer_pair_to_pool(primerpair, self._current_pool, msa_index)
 
     def get_seqs_in_pool(self) -> list[str]:
-        return [y for sublist in (x.all_seqs() for x in self._pools[self._current_pool]) for y in sublist]
-    
-    def get_leading_coverage_edge(self) -> tuple[int,int]:
+        return [
+            y
+            for sublist in (x.all_seqs() for x in self._pools[self._current_pool])
+            for y in sublist
+        ]
+
+    def get_leading_coverage_edge(self) -> tuple[int, int]:
         """This will return the furthest primer-trimmed region with coverage"""
         # This will crash if no primer has been added, but should not be called until one is
         return self._last_pp_added.rprimer.start
-    
-    def get_leading_amplicon_edge(self) -> tuple[int,int]:
+
+    def get_leading_amplicon_edge(self) -> tuple[int, int]:
         """This will return the furthest point of an amplicon"""
         # This will crash if no primer has been added, but should not be called until one is
         return max(self._last_pp_added.rprimer.ends())
-    
+
     def try_ol_primerpairs(self, all_pp_list, thermo_cfg, msa_index) -> bool:
         """
         This will try and add this primerpair into any valid pool.
@@ -190,54 +208,101 @@ class Scheme():
         """
         last_pool = self._last_pp_added.pool
         # Find what other pools to look in
-        pos_pools_indexes = [(last_pool + i)%self.n_pools for i in range(self.n_pools ) if (last_pool + i)%self.n_pools  != last_pool]
+        pos_pools_indexes = [
+            (last_pool + i) % self.n_pools
+            for i in range(self.n_pools)
+            if (last_pool + i) % self.n_pools != last_pool
+        ]
 
         # Create a hashmap of all sequences in each pool for quick look up
-        index_to_seqs: dict[int:list[str]] = {index: [y for sublist in (x.all_seqs() for x in self._pools[index]) for y in sublist] for index in pos_pools_indexes}
-        
+        index_to_seqs: dict[int : list[str]] = {
+            index: [
+                y
+                for sublist in (x.all_seqs() for x in self._pools[index])
+                for y in sublist
+            ]
+            for index in pos_pools_indexes
+        }
+
         # Find pp that could ol, depending on which pool
-        pos_ol_pp = [pp for pp in all_pp_list if min(pp.fprimer.starts()) < self.get_leading_coverage_edge() - self.cfg["min_overlap"] and pp.rprimer.start > self.get_leading_amplicon_edge() + self.cfg["min_overlap"]]
-        
-        #pos_ol_pp = get_pp_window(all_pp_list, fp_start=self.get_leading_coverage_edge() - self.cfg["min_overlap"] - self.cfg["amplicon_size_max"], fp_end=self.get_leading_coverage_edge() - self.cfg["min_overlap"], rp_end=self.get_leading_amplicon_edge() + self.cfg["min_overlap"])
+        pos_ol_pp = [
+            pp
+            for pp in all_pp_list
+            if min(pp.fprimer.starts())
+            < self.get_leading_coverage_edge() - self.cfg["min_overlap"]
+            and pp.rprimer.start
+            > self.get_leading_amplicon_edge() + self.cfg["min_overlap"]
+        ]
+
+        # pos_ol_pp = get_pp_window(all_pp_list, fp_start=self.get_leading_coverage_edge() - self.cfg["min_overlap"] - self.cfg["amplicon_size_max"], fp_end=self.get_leading_coverage_edge() - self.cfg["min_overlap"], rp_end=self.get_leading_amplicon_edge() + self.cfg["min_overlap"])
         # Sort the primerpairs depending on how good they are
-        pos_ol_pp.sort(key = lambda pp: ol_pp_score(pp.rprimer.start, len(pp.all_seqs()), self.get_leading_coverage_edge() - self.cfg["min_overlap"], self.cfg["min_overlap"]))
+        pos_ol_pp.sort(
+            key=lambda pp: ol_pp_score(
+                pp.rprimer.start,
+                len(pp.all_seqs()),
+                self.get_leading_coverage_edge() - self.cfg["min_overlap"],
+                self.cfg["min_overlap"],
+            )
+        )
 
         # For each primerpair
         for ol_pp in pos_ol_pp:
             # For each pool
             for pool_index in pos_pools_indexes:
-                # If the pool is empty 
+                # If the pool is empty
                 if not self._pools[pool_index]:
                     self.add_primer_pair_to_pool(ol_pp, pool_index, msa_index)
                     return True
-                
-                # If the last primer is from the same msa and does clash, skip it  
-                if self._pools[pool_index][-1].msa_index == msa_index and max(self._pools[pool_index][-1].rprimer.ends()) >= min(ol_pp.fprimer.starts()):
+
+                # If the last primer is from the same msa and does clash, skip it
+                if self._pools[pool_index][-1].msa_index == msa_index and max(
+                    self._pools[pool_index][-1].rprimer.ends()
+                ) >= min(ol_pp.fprimer.starts()):
                     continue
-                
+
                 # If the primer passes all the checks, make sure there are no interacts between new pp and pp in pool
-                if not all_inter_checker(ol_pp.all_seqs(), index_to_seqs.get(pool_index), thermo_cfg):
+                if not all_inter_checker(
+                    ol_pp.all_seqs(), index_to_seqs.get(pool_index), thermo_cfg
+                ):
                     self.add_primer_pair_to_pool(ol_pp, pool_index, msa_index)
                     return True
 
         # If non of the primers work, return false
         return False
-    
+
     def try_walk_primerpair(self, all_pp_list, thermo_cfg, msa_index) -> bool:
         """
         Find the next valid primerpair while walking forwards
         """
         last_pool = self._last_pp_added.pool
         # Find what other pools to look in, can look in same pool
-        pos_pools_indexes = [(last_pool + i)%self.n_pools for i in range(self.n_pools )]
+        pos_pools_indexes = [
+            (last_pool + i) % self.n_pools for i in range(self.n_pools)
+        ]
 
         # Create a hashmap of all sequences in each pool for quick look up
-        index_to_seqs: dict[int:list[str]] = {index: [y for sublist in (x.all_seqs() for x in self._pools[index]) for y in sublist] for index in pos_pools_indexes}
-        
+        index_to_seqs: dict[int : list[str]] = {
+            index: [
+                y
+                for sublist in (x.all_seqs() for x in self._pools[index])
+                for y in sublist
+            ]
+            for index in pos_pools_indexes
+        }
+
         # Find all posiable valid primerpairs
-        pos_walk_pp = [pp for pp in all_pp_list if pp.fprimer.end > (self.get_leading_coverage_edge() - (self.cfg["min_overlap"] * 2))]
+        pos_walk_pp = [
+            pp
+            for pp in all_pp_list
+            if pp.fprimer.end
+            > (self.get_leading_coverage_edge() - (self.cfg["min_overlap"] * 2))
+        ]
         # Sort walk primers by increasing start position
-        pos_walk_pp.sort(key = lambda pp: walk_pp_score(pp.fprimer.end, len(pp.all_seqs()), self._last_pp_added.end()))
+        pos_walk_pp.sort(
+            key=lambda pp: walk_pp_score(
+                pp.fprimer.end, len(pp.all_seqs()), self._last_pp_added.end()
+            )
+        )
 
         # For each primer, try each pool
         for walk_pp in pos_walk_pp:
@@ -246,28 +311,23 @@ class Scheme():
                 if not self._pools[pool_index]:
                     self.add_primer_pair_to_pool(walk_pp, pool_index, msa_index)
                     return True
-                
-                # If the last primer is from the same msa and does clash, skip it  
-                if self._pools[pool_index][-1].msa_index == msa_index and max(self._pools[pool_index][-1].rprimer.ends()) >= min(walk_pp.fprimer.starts()) :
+
+                # If the last primer is from the same msa and does clash, skip it
+                if self._pools[pool_index][-1].msa_index == msa_index and max(
+                    self._pools[pool_index][-1].rprimer.ends()
+                ) >= min(walk_pp.fprimer.starts()):
                     continue
 
                 # Check if the walking primer clashes with the primer already in the pool
-                if not all_inter_checker(walk_pp.all_seqs(), index_to_seqs.get(pool_index), thermo_cfg): 
+                if not all_inter_checker(
+                    walk_pp.all_seqs(), index_to_seqs.get(pool_index), thermo_cfg
+                ):
                     self.add_primer_pair_to_pool(walk_pp, pool_index, msa_index)
                     return True
-        
+
         return False
 
     def all_primers(self) -> list[PrimerPair]:
         all_pp = [pp for pool in (x for x in self._pools) for pp in pool]
-        all_pp.sort(key= lambda pp: (pp.msa_index, pp.amplicon_number))
+        all_pp.sort(key=lambda pp: (pp.msa_index, pp.amplicon_number))
         return all_pp
-    
-
-
-    
-
-    
-
-
-    
