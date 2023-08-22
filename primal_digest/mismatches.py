4ad49c97ce5e1e94f55a8ae19bdbbb00f72863d1
ChrisKent <chrisgkent@gmail.com>
2023-08-22 17:47:49 +0100

    Added unfinsihed mismatch engine

--- primal_digest/mismatches.py
+++ primal_digest/mismatches.py
@@ -2,13 +2,11 @@ import random
 import numpy as np
 from Bio import SeqIO
 from typing import Iterable
-from itertools import product
 
 import dbm.ndbm
 
 # Module imports
-from primal_digest.classes import FKmer, RKmer, AMBIGUOUS_DNA_COMPLEMENT
-from primal_digest.config import AMB_BASES, ALL_DNA
+from primal_digest.seq_functions import reverse_complement
 
 MUTATIONS = {
     "A": "CGT",
@@ -25,10 +23,19 @@ class MatchDB:
     delim between values:   b';'
     """
 
-    def __init__(self, path) -> None:
+    def __init__(self, path, msas_paths: list[str], kmer_size: int) -> None:
         self.db = dbm.ndbm.open(path, "n")
 
-    def write_unique(self, sequence, match: tuple[int, int]):
+        # Read in and digest each MSA
+        for msa_index, msa_path in enumerate(msas_paths):
+            records = SeqIO.parse(msa_path, "fasta")
+            # For each sequence in the MSA
+            for record in records:
+                self._digest_kmers_into_db(
+                    str(record.seq.upper()), kmer_size, msa_index
+                )
+
+    def _write_unique(self, sequence, match: tuple[int, int]):
         """This will only write unqiue values to the db"""
         match_bstr = f"{match[0]};{match[1]}".encode()
 
@@ -53,7 +60,7 @@ class MatchDB:
 
         return parsed_matches
 
-    def find_match(self, sequence) -> set[tuple]:
+    def find_match(self, sequence) -> set[list]:
         """
         Find all matches for a single sequence.
 
@@ -66,7 +73,6 @@ class MatchDB:
         if db_f_matches := self.read_matches(sequence):
             for match in db_f_matches:
                 matches.append(match + ["+"])
-
         if db_r_matches := self.read_matches(reverse_complement(sequence)):
             for match in db_r_matches:
                 matches.append(match + ["-"])
@@ -98,7 +104,7 @@ class MatchDB:
 
     def find_fkmer(
         self,
-        fkmer: FKmer,
+        fkmer,
         fuzzy: bool,
         kmersize: int,
         msaindex: int,
@@ -129,7 +135,7 @@ class MatchDB:
 
     def find_rkmer(
         self,
-        rkmer: FKmer,
+        rkmer,
         fuzzy: bool,
         kmersize: int,
         msaindex: int,
@@ -153,7 +159,7 @@ class MatchDB:
             return {
                 match
                 for match in matches
-                if match[1] != rkmer.end and match[0] == msaindex
+                if match[1] != rkmer.start and match[0] == msaindex
             }
         else:
             return matches
@@ -164,27 +170,39 @@ class MatchDB:
     def get(self, sequence, default=None):
         return self.db.get(sequence, default)
 
+    def _digest_kmers_into_db(self, seq: str, kmer_size: int, msa_index):
+        """
+        Digests a sequence into kmers and adds them to the db
 
-def hamming_dist(seq1, seq2) -> int:
-    return sum([x != y for x, y in zip(seq1, seq2)])
+        :param seq: The sequence to digest.
+        :param kmer_size: The Kmer size for digestion.
+        :param kmersize: The size of the kmer.
+        :param msa_index: The index of the MSA.
+        """
 
+        for i in range(len(seq) + 1 - kmer_size):
+            # Prevent a kmer starting on invalid base
+            if seq[i] in {"", "-"}:
+                continue
 
-def expand_ambs(seqs: Iterable[str]) -> set[set]:
-    """
-    Takes a list / set of strings and returns a set with all ambs expanded
-    It can return an empty set
-    """
-    returned_seq = set()
+            kmer = "".join(seq[i : i + kmer_size]).replace("-", "")
 
-    for seq in seqs:
-        # If there is any amb_bases in the seq
-        if {base for base in seq} & AMB_BASES:
-            expanded_seqs = set(map("".join, product(*map(ALL_DNA.get, seq))))
-            for exp_seq in expanded_seqs:
-                returned_seq.add(exp_seq)
-        else:
-            returned_seq.add(seq)
-    return returned_seq
+            if len(kmer) != kmer_size:
+                counter = 1
+
+                # Keep walking right until the kmer is the correct size or walks out of index
+                while counter + kmer_size + i < len(seq) and len(kmer) < kmer_size:
+                    new_base = seq[i + kmer_size + counter]
+
+                    # If the new base in valid
+                    if new_base != "-" and new_base != "":
+                        kmer += new_base
+
+                    counter += 1
+
+            # Check Kmer is correct size, and doesn't contain N
+            if len(kmer) == kmer_size and "N" not in kmer:
+                self._write_unique(kmer, (msa_index, i))
 
 
 def generate_single_mismatches(base_seq: str) -> set[str]:
@@ -199,97 +217,9 @@ def generate_single_mismatches(base_seq: str) -> set[str]:
     return return_seqs
 
 
-def reverse_complement(kmer_seq: str):
-    rev_seq = kmer_seq[::-1]
-    return "".join(AMBIGUOUS_DNA_COMPLEMENT[base.upper()] for base in rev_seq)
-
-
-def random_dna(size, seed) -> str:
-    random.seed(seed)
-    return "".join(random.choice("CGTA") for _ in range(size))
-
-
-def digest_kmers(
-    seq: str, kmer_size: int, msa_index
-) -> dict[str : set[tuple[int, int]]]:
-    """Return is (msa_index, start_index)"""
-
-    return_dict = {}
-    for i in range(len(seq) + 1 - kmer_size):
-        # Prevent a kmer starting on invalid base
-        if seq[i] in {"", "-"}:
-            continue
-
-        kmer = "".join(seq[i : i + kmer_size]).replace("-", "")
-
-        if len(kmer) != kmer_size:
-            counter = 1
-            # Keep walking right until the kmer is the correct size
-            while counter + kmer_size + i < len(seq) and len(kmer) < kmer_size:
-                kmer += seq[i + kmer_size + counter]
-                kmer.replace("-", "")
-                counter += 1
-
-        current_result = return_dict.get(kmer, set())
-        current_result.add((msa_index, i))
-        return_dict[kmer] = current_result
-
-    return return_dict
-
-
-def digest_kmers_db(
-    db: MatchDB, seq: str, kmer_size: int, msa_index
-) -> dict[str : set[tuple[int, int]]]:
-    """Return is (msa_index, start_index)"""
-
-    for i in range(len(seq) + 1 - kmer_size):
-        # Prevent a kmer starting on invalid base
-        if seq[i] in {"", "-"}:
-            continue
-
-        kmer = "".join(seq[i : i + kmer_size]).replace("-", "")
-
-        if len(kmer) != kmer_size:
-            counter = 1
-
-            # Keep walking right until the kmer is the correct size or walks out of index
-            while counter + kmer_size + i < len(seq) and len(kmer) < kmer_size:
-                new_base = seq[i + kmer_size + counter]
-
-                # If the new base in valid
-                if new_base != "-" and new_base != "":
-                    kmer += new_base
-
-                counter += 1
-
-        # Check Kmer is correct size, and doesn't contain N
-        if len(kmer) == kmer_size and "N" not in kmer:
-            db.write_unique(kmer, (msa_index, i))
-
-
-def digest_msa(msa_index: int, msa_array: np.ndarray, kmer_size=20) -> dict[str:list]:
-    return_dict: dict[str : list[int]] = {}
-
-    for row_index in range(msa_array.shape[0]):
-        row_dict = digest_kmers(
-            msa_array[row_index],
-            kmer_size=kmer_size,
-            msa_index=msa_index,
-        )
-        # merge the dicts
-        for k, v in row_dict.items():
-            if k in return_dict:
-                return_dict[k] = return_dict[k] | v
-            else:
-                return_dict[k] = v
-
-    return return_dict
-
-
 def detect_new_products(
     new_matches: set[tuple],
-    old_fmatches: set[tuple],
-    old_rmatches: set[tuple],
+    old_matches: set[tuple],
     product_size: int = 2000,
 ) -> bool:
     """
@@ -303,6 +233,15 @@ def detect_new_products(
             fmatches.add(newmatch)
         elif newmatch[2] == "-":
             rmatches.add(newmatch)
+
+    # Split the old matches in forward and reverse
+    old_fmatches = set()
+    old_rmatches = set()
+    for oldmatch in old_matches:
+        if oldmatch[2] == "+":
+            old_fmatches.add(oldmatch)
+        elif oldmatch[2] == "-":
+            old_rmatches.add(oldmatch)
 
     # Check the new fmatches against the old rmatches
     for fmatch in fmatches:
@@ -337,7 +276,7 @@ def detect_products(
     # TODO write a hash function that colides when a product is formed.
     ## Will make O(N) rather than than O(N^2)
 
-    # Split the matches in forward and reverse
+    # Split the mew matches in forward and reverse
     fmatches = set()
     rmatches = set()
     for match in matches:
@@ -364,65 +303,8 @@ def detect_products(
 def main():
     ARG_MSA = [
         "/Users/kentcg/primal-pannel/nibsc_viral_mutliplex/rhinovirus_a39--185907.fasta",
-        "/Users/kentcg/primal-pannel/nibsc_viral_mutliplex/human_astrovirus--1868658.fasta",
-        "/Users/kentcg/primal-pannel/nibsc_viral_mutliplex/human_respirovirus_1--12730.fasta",
-        "/Users/kentcg/primal-pannel/nibsc_viral_mutliplex/human_orthorubulavirus_2--2560525.fasta",
-        "/Users/kentcg/primal-pannel/nibsc_viral_mutliplex/RSVA_208893_final.fasta",
-        "/Users/kentcg/primal-pannel/nibsc_viral_mutliplex/human_adenovirus_2--10515.fasta",
-        "/Users/kentcg/primal-pannel/nibsc_viral_mutliplex/human_orthorubulavirus_4--2560526.fasta",
-        "/Users/kentcg/primal-pannel/nibsc_viral_mutliplex/human_respirovirus_3--11216.fasta",
-        "/Users/kentcg/primal-pannel/nibsc_viral_mutliplex/align_coxsackie_virus_b4--12073.fasta",
-        "/Users/kentcg/primal-pannel/nibsc_viral_mutliplex/human_metapneumovirus--162145.fasta",
-        "/Users/kentcg/primal-pannel/nibsc_viral_mutliplex/align_human_parechovirus--195055.fasta",
-        "/Users/kentcg/primal-pannel/nibsc_viral_mutliplex/human_adenovirus_41--10524.fasta",
-        "/Users/kentcg/primal-pannel/nibsc_viral_mutliplex/align_coronavirus-229E.fasta",
-        "/Users/kentcg/primal-pannel/nibsc_viral_mutliplex/align_human_alphaherpesvirus_2.fasta",
-        "/Users/kentcg/primal-pannel/nibsc_viral_mutliplex/align_human_gammaherpesvirus_4.fasta",
-        "/Users/kentcg/primal-pannel/nibsc_viral_mutliplex/human_alphaherpesvirus_1.fasta",
-        "/Users/kentcg/primal-pannel/nibsc_viral_mutliplex/align_human_betaherpesvirus_5.fasta",
-        "/Users/kentcg/primal-pannel/nibsc_viral_mutliplex/align_human_alphaherpesvirus_3.fasta",
     ]
-    matchdb = MatchDB("GRCh38_test")
-
-    # Read in and digest each MSA
-    for msa_index, msa_path in enumerate(ARG_MSA):
-        records = SeqIO.parse(msa_path, "fasta")
-        # For each sequence in the MSA
-        for record in records:
-            print(record.id)
-            digest_kmers_db(matchdb, str(record.seq.upper()), 20, msa_index)
-
-    fkmer = FKmer(
-        200,
-        seqs={
-            "AAAACGGCAACTGCTGATATGCTC",
-            "CGGTAACTGCTGATATGCTC",
-            "CGGTAACTGCTGATATGCTC",
-            "CGGCAACTGCTGATATGCTC",
-            "CGGCAACTGCTGATATGCTC",
-            "CGGTAACTGCTGATATGCTC",
-        },
-    )
-    Rkmer = RKmer(
-        200,
-        seqs={
-            "AAAACGGCAACTGCTGATATGCTC",
-            "CGGTAACTGCTGATATGCTC",
-            "CGGTAACTGCTGATATGCTC",
-            "CGGCAACTGCTGATATGCTC",
-            "CGGCAACTGCTGATATGCTC",
-            "CGGTAACTGCTGATATGCTC",
-        },
-    )
-
-    # Match Kmers
-    print(
-        matchdb.find_fkmer(
-            fkmer=fkmer, fuzzy=True, remove_expected=False, msaindex=0, kmersize=20
-        )
-    )
-
-    print(detect_new_products({(0, 100, "+")}, {(0, 100, "+")}, {(0, 200, "-")}))
+    matchdb = MatchDB("test", ARG_MSA, 20)
 
 
 if __name__ == "__main__":
