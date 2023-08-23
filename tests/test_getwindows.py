f0420586501d5c4831800c30ce677334418faaf2
ChrisKent <chrisgkent@gmail.com>
2023-08-23 12:04:35 +0100

    Added tests for binary searches

--- tests/test_getwindows.py
+++ tests/test_getwindows.py
@@ -31,5 +31,27 @@ class Test_GetRWindowFAST2(unittest.TestCase):
         self.assertEqual(result_rkmers, expected_rkmers)
 
 
+class Test_GetPpWindow(unittest.TestCase):
+    def test_get_pp_window_ol(self):
+        fkmers = [FKmer(end, "A") for end in range(20, 30)]
+        rkmers = [RKmer(start, "A") for start in range(90, 100)]
+
+        primerpairs = []
+        for fkmer in fkmers:
+            for rkmer in rkmers:
+                primerpairs.append(PrimerPair(fkmer, rkmer, 0))
+        primerpairs.sort(key=lambda pp: (pp.fprimer.end, pp.rprimer.start))
+
+        # Get all kmers that start between 40 and 50
+        expected_pos_ol_pp = [
+            pp
+            for pp in primerpairs
+            if pp.fprimer.end >= 22 and pp.fprimer.end <= 28 and pp.rprimer.start >= 95
+        ]
+
+        result_pp = get_pp_window(primerpairs, 22, 28, 95)
+        self.assertEqual(expected_pos_ol_pp, result_pp)
+
+
 if __name__ == "__main__":
     unittest.main()
