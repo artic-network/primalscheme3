539b8798886037da2b207586bf45bbddefeed520
ChrisKent <chrisgkent@gmail.com>
2023-08-24 12:54:26 +0100

    Updates score function

--- primal_digest/primer_pair_score.py
+++ primal_digest/primer_pair_score.py
@@ -1,12 +1,15 @@
 from math import sqrt
 
-def ol_pp_score(pp_r_start: int, pp_n_p: int,leading_edge: int, min_overlap: int) -> int:
-    return -(pp_r_start - min_overlap - leading_edge) ** 2 / sqrt(pp_n_p)
+
+def ol_pp_score(pp_r_start: int, pp_n_p: int, leading_edge: int, cfg) -> int:
+    """
+    Higher score is better
+    """
+    dist_extend = pp_r_start - cfg["min_overlap"] - leading_edge
+    prop_extended = dist_extend / cfg["amplicon_size_max"]
+
+    return prop_extended**2 / sqrt(pp_n_p)
+
 
 def walk_pp_score(pp_f_end: int, pp_n_p: int, leading_edge: int) -> int:
-    return (pp_f_end- leading_edge) * sqrt(pp_n_p)
-
-    
-
-
-
+    return (pp_f_end - leading_edge) * sqrt(pp_n_p)
