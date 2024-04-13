10bd2cb29ae418e6455356e25c4eecf331b7a0b3
ChrisKent <chrisgkent@gmail.com>
2024-04-13 14:05:31 +0100

    Added a stout option to setup_logger to make it easier to run wihtout an output file

--- primalscheme3/core/logger.py
+++ primalscheme3/core/logger.py
@@ -8,22 +8,30 @@ from loguru import logger
 log = logger.opt(colors=True)
 
 
-def setup_loger(OUTPUT_DIR: pathlib.Path):
+def setup_loger(OUTPUT_DIR: pathlib.Path | None):
     """
     Sets up the logger for the application
-    :param OUTPUT_DIR: The output directory
+    :param OUTPUT_DIR: The output directory or None for stdout only
     :return: The loguru logger
     """
     ## Set up the loggers
     log.remove()  # Remove default stderr logger
-    # Add the deep log file
-    log.add(
-        OUTPUT_DIR / "work/file.log",
-        colorize=False,
-        format="{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}",
-        enqueue=True,
-    )
+
+    if OUTPUT_DIR is not None:
+        # Add the deep log file
+        log.add(
+            OUTPUT_DIR / "work/file.log",
+            colorize=False,
+            format="{time:YYYY-MM-DD at HH:mm:ss}|{level}|{message}",
+            enqueue=True,
+        )
+
     # Add the nice stdout progress
-    log.add(sys.stdout, colorize=True, format="{message}", level="INFO")
+    log.add(
+        sys.stdout,
+        colorize=True,
+        format="{time:HH:mm:ss}|{level}| {message}",
+        level="INFO",
+    )
 
     return log
