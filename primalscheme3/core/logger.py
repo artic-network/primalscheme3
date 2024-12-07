4732925c5aeb817ecb57462ba4b6a308b3b5d4d8
ChrisKent <chrisgkent@gmail.com>
2024-12-07 12:53:45 +0000

    Updated loggers

--- primalscheme3/core/logger.py
+++ primalscheme3/core/logger.py
@@ -1,42 +1,3 @@
-# Sets up the logger for the application
-
-import pathlib
-import sys
-
-from loguru import logger
-
-log = logger.opt(colors=True)
-
-
-def setup_logger(OUTPUT_DIR: pathlib.Path | None):
-    """
-    Sets up the logger for the application
-    :param OUTPUT_DIR: The output directory or None for stdout only
-    :return: The loguru logger
-    """
-    ## Set up the loggers
-    log.remove()  # Remove default stderr logger
-
-    if OUTPUT_DIR is not None:
-        # Add the deep log file
-        log.add(
-            OUTPUT_DIR / "work/file.log",
-            colorize=False,
-            format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {message}",
-            enqueue=True,
-        )
-
-    # Add the nice stdout progress
-    log.add(
-        sys.stdout,
-        colorize=True,
-        format="{time:HH:mm:ss}|{level}| {message}",
-        level="INFO",
-    )
-
-    return log
-
-
 def setup_rich_logger(logfile: str | None = None):
     import logging
 
@@ -58,8 +19,12 @@ def setup_rich_logger(logfile: str | None = None):
     logging.basicConfig(
         format="%(message)s",
         datefmt="[%X]",
-        handlers=handlers,
-        level=logging.DEBUG,
     )
-    log = logging.getLogger("rich")
+    log = logging.getLogger(f"rich-{hash(logfile)}")
+    log.setLevel(logging.DEBUG)
+
+    # Add the handlers
+    for handler in handlers:
+        log.addHandler(handler)
+
     return log
